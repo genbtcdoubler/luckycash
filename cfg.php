a1655e32b34aab577d6722689b0ea64745773018
genbtcdoubler <79389495+genbtcdoubler@users.noreply.github.com>
2021-03-01 02:15:12 -0800

    Add files via upload

---
+++ cfg.php
@@ -0,0 +1,29 @@
+<?php
+
+   $ua=array();
+   $ua[]="Accept: application/json";
+   $ua[]="x-access-token: xxxxxxxxxxxx";
+   $ua[]="Content-type: application/json";
+   $ua[]="User-Agent: xxxxxxxxxxxxxxxx";
+   $ua[]="Host: appli.picturehit.cool";
+   $ua[]="Connection: Keep-Alive";
+   
+// Masukan data  getUser
+$login = 'xxxxxxxxxxxxx';
+
+// Masukan data updatePushToken
+$update = 'xxxxxxxxxxxx';
+
+// Masukan data receive Delta Credits
+$video = 'xxxxxxxxxxxxxxx';
+
+// Masukan data launch Wheel
+$spin =  'xxxxxxxxxxxxxxxx';
+
+//  Masukan data result wheel
+$wheel = 'xxxxxxxxxxxxxxx';
+
+
+
+
+
